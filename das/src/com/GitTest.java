0ef1b5d246924e9bda8b5a0a91b66d3d2868e987
caesar76 <tomotion@naver.com>
2018-06-19 10:00:27 +0900

    변경저장

--- das/src/com/GitTest.java
+++ das/src/com/GitTest.java
@@ -4,5 +4,16 @@ public class GitTest {
 	
 	public static void main(String[] args){
 		System.out.println("Git Start");
+		
+		for(int i = 1 ; i <=100 ; i++){
+			System.out.print(i + "ÀÇ ¾à¼ö :");
+			
+			for(int j = 1 ; j <= i ; j++){
+				if(i % j == 0){
+					System.out.print(j +",");
+				}
+			}
+			System.out.println();
+		}
 	}
 }
